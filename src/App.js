1ef4bdf5b2b267f51d19a0f9b5ac4f71b6d51526
Mohit Ande <andemohit@gmail.com>
2020-03-28 23:53:59 +0530

    imported header and about component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './components/header/header';
+import About from './components/about/about';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        Asheesh
-      </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <footer>
-        This is footer tag
-      </footer>
-    </div>
+    <React.Fragment>
+      <Header />
+      <About />
+    </React.Fragment>
   );
 }
 
