594bf1753f5b19fee7fcf4eb89eb2c3feb8601c3
Mohit Ande <andemohit@gmail.com>
2020-03-28 22:41:46 +0530

    added About Component

---
+++ src/components/about/about.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class About extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="w3-about" id="about">
+                    <div className="container"></div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
